594fd4df1ca767485b17d4d1ba8deafc92bc402b
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-08-07 00:49:16 -0300

    Erro do Knex

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,18 +1,15 @@
-// Update with your config settings.
-
 module.exports = {
-    client: 'postgresql',
-    connection: {
-      database: 'knowledge',
-      user:     'postgres',
-      password: '123456'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-
+  client: 'postgresql',
+  connection: {
+    database: 'knowledge',
+    user:     'postgres',
+    password: 'senha'
+  },
+  pool: {
+    min: 2,
+    max: 20
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 };
--- backend/migrations/20200806233719_create_table_users.js
+++ backend/migrations/20200806233719_create_table_users.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', table => {
+        table.increment('id').primary()
+        table.string('name').notNull()
+        table.string('email').notNull()
+        table.string('password').notNull()
+        table.boolean('admin').notNull().defaultTo(false)
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('users')
 };
