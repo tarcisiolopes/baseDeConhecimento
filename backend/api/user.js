4a032622b327d9f77a5c9da5de94ba7ffec74ead
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-08-11 22:39:21 -0300

    Obter Usuário por Id

--- backend/api/user.js
+++ backend/api/user.js
@@ -57,6 +57,16 @@ module.exports = app => {
             .then(users => res.json(users))
             .catch(err => res.status(500).send(err))
     }
+    
+    const getById = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            // .whereNull('deletedAt')
+            .where({ id: req.params.id })
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
+    }
 
-    return { save, get }
+    return { save, get, getById }
 }
