353ab8c224b5872d46137e14743cfaf6e5407209
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-08-17 10:41:46 -0300

    Estrutura Inicial do Template

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+      <span>Realizado por <strong>T</strong>arcisio <strong>L</strong>opes.</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer{
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
