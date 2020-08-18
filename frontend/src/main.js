0544a568a33b96eb31f2e64bade3096c224ea57c
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-08-17 21:57:25 -0300

    Componente Home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORÁRIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlRhcmNpc2lvIExvcGVzIEFsYmVybmF6IEdvbWVzIiwiZW1haWwiOiJ0YXJjaXNpb2xvcGVzZ29tZXNAZ21haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTU5NzcxMDg5OCwiZXhwIjoxNTk3OTcwMDk4fQ.5B5dAnX9-Ulub2I9r2zyF9Cm93_7hUeWmhEaUXrW3Oo'
+
 new Vue({
   store,
   router,
