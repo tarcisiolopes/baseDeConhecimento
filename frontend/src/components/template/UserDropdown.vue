0275408e79d96c9ef1767b0c033ba858678ec405
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-08-18 14:20:21 -0300

    Autenticação: Logout

--- frontend/src/components/template/UserDropdown.vue
+++ frontend/src/components/template/UserDropdown.vue
@@ -11,7 +11,7 @@
       <router-link to="/admin">
         <i class="fa fa-cogs"></i>Administração
       </router-link>
-      <a href>
+      <a href @click.prevent="logout">
         <i class="fa fa-sign-out"></i>Sair
       </a>
     </div>
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { userKey } from "@/global";
 import { mapState } from "vuex";
 import Gravatar from "vue-gravatar";
 
@@ -26,6 +27,13 @@ export default {
   name: "UserDropdown",
   components: { Gravatar },
   computed: mapState(["user"]),
+  methods: {
+    logout() {
+      localStorage.removeItem(userKey);
+      this.$store.commit("setUser", null);
+      this.$router.push({ name: "auth" });
+    },
+  },
 };
 </script>
 
